eb9c1bc3e1f11c036d53036515d1b492f0fd27ee
Anna Zuma <3umchik24@mail.ru>
2023-06-08 21:17:39 +0300

    6

--- Lessons/webinar1/Ex2.java
+++ Lessons/webinar1/Ex2.java
@@ -1,33 +1,33 @@
-package Lessons.webinar1;
-
-public class Ex2 {
-    public static void main(String[] args) {
-        int[] nums = new int[]{3,2,5,3};
-        int val = 3;
-        System.out.println(getReturnA(nums, val));
-
-    }
-
-//Дан массив nums = [3,2,5,3] и число val = 3.
-//Если в массиве есть числа,
-//равные заданному,
-//нужно перенести эти элементы в конец массива.
-//Таким образом,
-//первые несколько (или все) элементов массива должны быть отличны от заданного,
-//а остальные - равны ему.
-    static int getResultA(int[] nums, int val){
-        int[] temp = new int[]
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 3){
-
-            }
-        }
-
-    }
-
-    static void start() {
-
-    }
-
-
-}
+//package Lessons.webinar1;
+//
+//public class Ex2 {
+//    public static void main(String[] args) {
+//        int[] nums = new int[]{3,2,5,3};
+//        int val = 3;
+//        System.out.println(getReturnA(nums, val));
+//
+//    }
+//
+////Дан массив nums = [3,2,5,3] и число val = 3.
+////Если в массиве есть числа,
+////равные заданному,
+////нужно перенести эти элементы в конец массива.
+////Таким образом,
+////первые несколько (или все) элементов массива должны быть отличны от заданного,
+////а остальные - равны ему.
+//    static int getResultA(int[] nums, int val){
+//        int[] temp = new int[]
+//        for (int i = 0; i < nums.length; i++) {
+//            if (nums[i] == 3){
+//
+//            }
+//        }
+//
+//    }
+//
+//    static void start() {
+//
+//    }
+//
+//
+//}
